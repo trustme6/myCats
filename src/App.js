f1e846706c07d54b28788d70cf28cda1a8912ec7
trustme6 <trustmeeverydayandnow@gmail.com>
2023-05-16 14:36:35 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
 
 function App() {
+  const [catPhoto, setCatPhoto] = useState("");
+  const catPhotoCaption = "Here is cat!";
+
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * 10);
+  };
+
+
+  const getRandomCatPhoto = () => {
+    const randomIndex = getRandomInt();
+    const catPhotosFolder = process.env.PUBLIC_URL + "/images";
+    const catPhotos = ["frida-1.jpg", "frida-2.jpg", "frida-3.jpg","vishnya-1.jpg", "vishnya-2.jpg", "vishnya-3.jpg", "shishka-1.jpg", "shishka-2.jpg", "shishka-3.jpg"]; 
+
+
+    const randomCatPhotoPath = catPhotosFolder + "/" + catPhotos[randomIndex];
+    return randomCatPhotoPath;
+  };
+
+
+  useEffect(() => {
+    const randomCatPhoto = getRandomCatPhoto();
+    setCatPhoto(randomCatPhoto);
+  }, []);
+
+  const handleCatPhotoChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setCatPhoto(URL.createObjectURL(file));
+    }
+  };
+
+  const handleButtonClick = (buttonNumber) => {
+    if (buttonNumber === 1) {
+      const randomCatPhoto = getRandomCatPhoto();
+      setCatPhoto(randomCatPhoto);
+    } else {
+      console.log(`Нажата кнопка ${buttonNumber}`);
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      <button onClick={() => handleButtonClick(1)}>myCats</button>
+      <button onClick={() => handleButtonClick(1)}>Home</button>
+      <button onClick={() => handleButtonClick(3)}>About</button>
+      <button onClick={() => handleButtonClick(4)}>Dark mode</button>
+      <button onClick={() => handleButtonClick(1)}>Give me another cat!</button>
+      <button onClick={() => handleButtonClick(6)}>Upload</button>
+      <div>
+        <h2>{catPhotoCaption}</h2>
+        <input type="file" accept="image/*" onChange={handleCatPhotoChange} />
+        {catPhoto && <img src={catPhoto} alt="Кошка" />}
+      </div>
     </div>
   );
 }
