d3b0f61e34cd764c55596dc02f03cb22d49f8d35
trustme6 <trustmeeverydayandnow@gmail.com>
2023-05-18 18:54:22 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
 
-function App() {
+export function Home() {
   const [catPhoto, setCatPhoto] = useState("");
-  const [showAboutPage, setShowAboutPage] = useState(false);
-  
-  
+  useEffect(() => {
+    const randomCatPhoto = getRandomCatPhoto();
+    setCatPhoto(randomCatPhoto);
+  }, []);
+
+  const handleCatPhotoChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setCatPhoto(URL.createObjectURL(file));
+    }
+  };
 
   const catPhotoCaption = "Here is cat!";
 
@@ -32,106 +41,88 @@ function App() {
     return randomCatPhotoPath;
   };
 
-  useEffect(() => {
-    const randomCatPhoto = getRandomCatPhoto();
-    setCatPhoto(randomCatPhoto);
-  }, []);
-
-  const handleCatPhotoChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setCatPhoto(URL.createObjectURL(file));
-    }
-  };
-  const handleButtonClick = (buttonNumber) => {
-    if (buttonNumber === 1) {
-      const randomCatPhoto = getRandomCatPhoto();
-      setCatPhoto(randomCatPhoto);
-      setShowAboutPage(false);
-    } else if (buttonNumber === 2) {
-      setShowAboutPage(true);
-    } else {
-      console.log(`Нажата кнопка ${buttonNumber}`);
-    }
-  };
-
-  const renderAboutPage = () => {
-    return (
-      <div>
-        <h1>About</h1>
-        <div>
-          <h2>Shishka</h2>
-          <img
-            src={process.env.PUBLIC_URL + "/images/shishka-1.jpg"}
-            alt="Cat 1"
-          />
-          <h2>Silent killer</h2>
-          <p>
-            She will always support in difficult times. She has a complex
-            character that makes life more interesting. Can scare a sudden
-            appearance from around the corner. The oldest of the cats.
-          </p>
-        </div>
-        <div>
-          <h2>Vishnya</h2>
-          <img
-            src={process.env.PUBLIC_URL + "/images/vishnya-2.jpg"}
-            alt="Cat 2"
-          />
-          <h2>Good but crazy</h2>
-          <p>
-            Wakes up better than any alarm clock. Extremely affectionate and
-            endearing. She looks at the world more widely because of her big
-            eyes and plays with pleasure even with people who are not shy.
-            Sometimes she sleeps in positions that deny physics. One of the
-            little sisters of Shishka.{" "}
-          </p>
-        </div>
-        <div>
-          <h2>Frida</h2>
-          <img
-            src={process.env.PUBLIC_URL + "/images/frida-3.jpg"}
-            alt="Cat 3"
-          />
-          <h2>Cup of friducchio</h2>
-          <p>
-            Purrs like a tractor. The plumpest of the cats with the noblest fur.
-            Loves being petted in the shower. An efficient predator that can
-            prey on anything that moves, but often sleeps sweetly on her
-            cardboard couch, in which otherwise she does not fit completely.One
-            of the little sisters of Shishka
-          </p>
-        </div>
-      </div>
-    );
-    
-  };
-
-  
-
   return (
     <div>
-    <button onClick={() => handleButtonClick(1)}>MyCats</button>
-    <button onClick={() => handleButtonClick(1)}>Home</button>
-    <button onClick={() => handleButtonClick(2)}>About</button>
-    <button onClick={() => handleButtonClick(3)}>Dark mode</button>
-    <button onClick={() => handleButtonClick(1)}>Give me another cat!</button>
-    <button onClick={() => handleButtonClick(5)}>Upload</button>
-    {showAboutPage ? (
-      renderAboutPage()
-    ) : (
       <div>
-        <h2>{catPhotoCaption}</h2>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleCatPhotoChange}
-        />
-        {catPhoto && <img src={catPhoto} alt="Кошка" />}
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/about">About</Link>
+        </button>
       </div>
-    )}
-  </div>
-);
+      <h2>{catPhotoCaption}</h2>
+      <input type="file" accept="image/*" onChange={handleCatPhotoChange} />
+      {catPhoto && <img src={catPhoto} alt="Cat" />}
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div>
+      <div>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/about">About</Link>
+        </button>
+      </div>
+      <h1>About</h1>
+      <div>
+        <h2>Shishka</h2>
+        <img
+          src={process.env.PUBLIC_URL + "/images/shishka-1.jpg"}
+          alt="Cat 1"
+        />
+        <h2>Silent killer</h2>
+        <p>
+          She will always support in difficult times. She has a complex
+          character that makes life more interesting. Can scare a sudden
+          appearance from around the corner. The oldest of the cats.
+        </p>
+      </div>
+      <div>
+        <h2>Vishnya</h2>
+        <img
+          src={process.env.PUBLIC_URL + "/images/vishnya-2.jpg"}
+          alt="Cat 2"
+        />
+        <h2>Good but crazy</h2>
+        <p>
+          Wakes up better than any alarm clock. Extremely affectionate and
+          endearing. She looks at the world more widely because of her big eyes
+          and plays with pleasure even with people who are not shy. Sometimes
+          she sleeps in positions that deny physics. One of the little sisters
+          of Shishka.{" "}
+        </p>
+      </div>
+      <div>
+        <h2>Frida</h2>
+        <img src={process.env.PUBLIC_URL + "/images/frida-3.jpg"} alt="Cat 3" />
+        <h2>Cup of friducchio</h2>
+        <p>
+          Purrs like a tractor. The plumpest of the cats with the noblest fur.
+          Loves being petted in the shower. An efficient predator that can prey
+          on anything that moves, but often sleeps sweetly on her cardboard
+          couch, in which otherwise she does not fit completely.One of the
+          little sisters of Shishka
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { About, Home } from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About/>
+  }
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
+
 );
 
 
