02be2238a8fd3701a01bfd4528a39cf98c7edf52
trustme6 <trustmeeverydayandnow@gmail.com>
2023-06-16 16:43:03 +0300

    refactoring

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -20,13 +20,21 @@ export const Home = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-const {isSmallScreen} = useBreakpoints(); 
+  const { isSmallScreen } = useBreakpoints();
 
   const [isMenuButtonVisible, setIsMenuButtonVisible] = useState(isSmallScreen);
+
+  const [catPhotos, setCatPhotos] = useState<string[]>([]);
 
   const logoImage = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
 
   const getRandomCatPhoto = async () => {
+    const randomIndex = Math.floor(Math.random() * catPhotos.length);
+    setRandomCatPhoto(catPhotos[randomIndex]);
+    setRandomCatPhotoKey(Date.now().toString());
+  };
+
+  const getCatPhotos = async () => {
     const catPhotosFolder = "images";
     const storageRef = ref(storage, catPhotosFolder);
     const photoList = await listAll(storageRef);
@@ -36,30 +44,30 @@ const {isSmallScreen} = useBreakpoints();
         return downloadURL;
       })
     );
-    const randomIndex = Math.floor(Math.random() * photoUrls.length);
-    setRandomCatPhoto(photoUrls[randomIndex]);
-    setRandomCatPhotoKey(Date.now().toString());
+
+    setCatPhotos(photoUrls);
   };
 
-useEffect (()=>{
-if(isSmallScreen){
-  setIsMenuButtonVisible(true); 
-}else{
-  setIsModalOpen(false);
-  setIsMenuButtonVisible(false);
-}
-},[isSmallScreen])
+  useEffect(() => {
+    if (isSmallScreen) {
+      setIsMenuButtonVisible(true);
+    } else {
+      setIsModalOpen(false);
+      setIsMenuButtonVisible(false);
+    }
+  }, [isSmallScreen]);
 
   useEffect(() => {
-    getRandomCatPhoto();
+    getCatPhotos();
   }, []);
-
 
   useEffect(() => {
     localStorage.setItem("darkMode", String(isDarkMode));
   }, [isDarkMode]);
+  useEffect(() => {
+    getRandomCatPhoto();
+  }, [catPhotos.length]);
 
- 
   const catPhotoCaption = "Here is a cat!";
 
   const handleDarkModeToggle = () => {
@@ -70,20 +78,23 @@ if(isSmallScreen){
     setIsModalOpen(!isModalOpen);
   };
 
-  
   return (
     <>
       <Global styles={isDarkMode ? darkStyles : lightStyles} />
       <div className="navbar">
         <div className="navbar-top" role="navigation">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" onClick={() => getRandomCatPhoto()}>
+            <Link
+              to="/"
+              className="navbar-item"
+              onClick={() => getRandomCatPhoto()}
+            >
               <img src={process.env.PUBLIC_URL + logoImage} alt="Icon" />
               <strong>MyCats</strong>
             </Link>
           </div>
           <div className="navbar-menu">
-          <div className="navbar-start">
+            <div className="navbar-start">
               {!isSmallScreen && (
                 <MenuItems
                   isSmallScreen={isSmallScreen}
@@ -102,7 +113,16 @@ if(isSmallScreen){
           </div>
         </div>
 
-        <Modal isModalOpen={isModalOpen} menuItems={<MenuItems isSmallScreen={isSmallScreen} isDarkMode={isDarkMode} handleDarkModeToggle={handleDarkModeToggle} />} />
+        <Modal
+          isModalOpen={isModalOpen}
+          menuItems={
+            <MenuItems
+              isSmallScreen={isSmallScreen}
+              isDarkMode={isDarkMode}
+              handleDarkModeToggle={handleDarkModeToggle}
+            />
+          }
+        />
 
         <section className="cat">
           <div className="title-centered">
